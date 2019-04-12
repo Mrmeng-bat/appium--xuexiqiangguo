83418d3017791f1cd3874c1fb4d80eb21a1045e5
Mrmeng-bat <984536265@qq.com>
2019-04-12 18:34:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# appium+xuexiqiangguo
+##第一步，如何配置环境
+这个脚本是使用python3+appium+夜神安卓虚拟机来完成模拟学习学习强国的脚本。
+__首先重要提示__:由于appium依赖于android环境和java环境以及node.js 所以环境的配置比较麻烦。需要很长的时间，但是一劳永逸。
+1. 首先配置java环境 
+   1. [java安装下载地址](https://www.oracle.com/technetwork/java/javase/downloads/index.html)
+   2. 我这里是使用的12版本的，按照安装程序的提示一步步安装注意路径里面不要有中文路径
+   3. 设置三个环境变量,我的电脑>选择“属性”->“高级”->“环境变量”->“系统变量”->“新建”
+   4. 新建变量名：JAVA_HOME，变量值：C:\Program Files\Java\jdk\ 注意：变量值这里改为你自己的安装目录
+   5. 打开PATH，添加变量值：%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin
+   6. 新建变量名：CLASSPATH，变量值：.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar
+   备注：
+	1) .表示当前路径，%JAVA_HOME%就是引用前面指定的JAVA_HOME；cat，eclipse等的运行都需要依靠此变量。
+	3) PATH使得系统可以在任何路径下识别java命令。
+	2) JAVA_HOME指明JDK安装路径，此路径下包括lib，bin，jre等文件夹，tom
+	4) CLASSPATH为java加载类(class or lib)路径，只有类在classpath中，java命令才能识别。
+	5）按快捷键win+r，在运行内输入cmd，并输入Java，回车，如果出来的是些用法中文，那说明这是正确的，在接下来输入javac，回车，如果出现些用法中文，说明是环境变量配好了。
+2. 配置sdk
+   1. 通过android的官网或者baidu搜索android sdk下载android sdk并解压放到对应的目录下
+   2. 双击打开SDK Manage.exe，选择需要安装的API版本和对应的工具包进行安装
+   3. 安装完成后，新建环境变量ANDROID_HOME，变量值为sdk的路径：E:\Android\android-sdk 同样路径改为你实际安装路径
+   4. 在Path变量中增加两个路径：E:\Android\android-sdk\platform-tools和E:\Android\android-sdk\tools
+   5. 配置了环境变量后，在命令行窗口中运行adb -version，没有报错就说明安装成功了
+3. 安装python3.4 这个在网上随便找一个安装好就可以。
+4. 安装Node.js访问 https://nodejs.org/en/download/，下载node.js，根据操作系统下载对应的安装包。
+   1. 依旧默认安装即可
+   2. 安装完成后，会自动将路径加入到环境变量中，在命令行窗口中运行node -v，如果出现版本号，表示安装正常
+5. 安装Appium
+   1. 打开命令行输入npm install -g appium安装appium
+   2. 建议顺便安装一下appium-doctor，通过运行appium-doctor命令可以快速检查appium的环境问题。
+   3. 如果安装成功，那么就可以通过 appium 命令启动appium server
+6.安装appium - python - client 和numpy
+   1. 打开命令行输入pip install Appium-Python-Client
+   2. 打开命令行输入pip install numpy
+   2. 显示安装成功即可
+7. 如果之前的环境问题全部搞定了。这里我们离成功基本上就搞定了百分之99的问题了
+   1. 安装夜神模拟器，百度夜神模拟器直接安装即可
+   2. 由于adb版本的不同，会出现appium报错，这里打开sdk安装目录下的\sdk\platform-tools下的adb.exe,把他复制到并覆盖到夜神模拟器下的\Nox\bin adb.exe ,在复制一份改名为nox_adb.exe。确保夜神模拟器的adb版本和sdkadb版本相同
+8. 在夜神模拟器应用商店中下载安装好学习强国并登陆
+9. 下载本项目，将学习强国测试.py第130行更改为你的夜神模拟器的启动目录
+10. 运行python 强国测试.py测试是否正常运行，正常的话将添加windos计划任务为每天固定时间运行即可。
+11. 哎，搞了这么多都是我的心酸路，不指望别人能有耐性配置这么多环境。但是自己用感觉还是爽的飞起。哈哈
---
+++ 学习强国测试.py
@@ -0,0 +1,153 @@
+"""
+info:
+author:Mrmeng
+github:https://github.com/Mrmeng-bat/
+update_time:2019-3-24
+"""
+import os
+import subprocess
+from appium import webdriver
+import time
+import numpy as np
+
+all_of_text_list = np.load ("text_db.npy").tolist()
+all_of_vod_list = np.load ("move_db.npy").tolist()
+
+
+
+def init_driver():   #return desired_caps字典
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.1.1'
+    desired_caps['deviceName'] = 'Nexus One'
+    desired_caps['appPackage'] = 'cn.xuexi.android'
+    desired_caps['appActivity'] = 'com.alibaba.android.rimet.biz.SplashActivity'
+    desired_caps['noSign'] = True
+    desired_caps['noReset'] = True
+    desired_caps['newCommandTimeout'] = 3600
+    return desired_caps
+
+def swipe_y(driver): #x下滑动作
+    y = driver.get_window_size()['height']
+    driver.swipe(0, 5/10*y, 0, 2/10*y, 200)
+
+def swipe_y2(driver): #x下滑动作
+    y = driver.get_window_size()['height']
+    for i in range(2):
+        for j in range (30):
+            driver.swipe(0, 3/10*y, 0, 2/10*y, 500)
+            time.sleep (1)
+def swipe_x(driver): #x下滑动作
+    x = driver.get_window_size()['width']
+    driver.swipe(2/10*x, 0, 6/10*x, 0, 200)
+def move_to_index(driver): #转到我的学习
+    driver.find_element_by_id("cn.xuexi.android:id/home_bottom_tab_icon_large").click()
+
+def move_to_vod(driver):
+    driver.find_element_by_xpath('//android.widget.TextView[@text="视频学习"]').click()
+
+def move_to_my_study(driver):
+    driver.find_element_by_xpath('//android.widget.TextView[@text="我的"]').click()
+
+def move_to_shi_ping(driver):
+    driver.find_element_by_id("cn.xuexi.android:id/home_bottom_tab_icon_large").click()
+    driver.find_element_by_xpath('//android.widget.TextView[@text="时评"]').click()
+
+def get_text_list(driver):   #返回未读文章列表
+    text_list = []
+    lists = driver.find_elements_by_class_name ('android.widget.TextView')
+    for d_text in lists:
+        if len(text_list) >6:
+            break
+        if len(d_text.text)<11:
+            continue
+        if d_text.text not in  all_of_text_list :
+            all_of_text_list.append(d_text.text )
+            text_list.append(d_text)
+            print("找到未读文章：",d_text.text)
+    return text_list
+
+def get_vod_list(driver):   #返回未看视频列表
+    vod_list = []
+    lists = driver.find_elements_by_id('cn.xuexi.android:id/general_card_title_id')
+    for d_vod in lists:
+        if len(vod_list) >6:
+            break
+        if d_vod.text not in  all_of_vod_list:
+            all_of_vod_list.append(d_vod.text )
+            vod_list.append(d_vod )
+            print("找到未看视频：",d_vod.text)
+    return vod_list
+
+def look_text(driver):
+    lists=[]
+    time.sleep(2)
+    move_to_shi_ping(driver)
+    time.sleep(2)
+    while len(lists)<7:
+        time.sleep(2)
+        for_list = get_text_list(driver)
+        for d_text in for_list:
+            if len(lists)>6:
+                break
+            d_text.click()
+            time.sleep(2)
+            print('正在打开',d_text.text)
+            swipe_y2(driver)
+            time.sleep(30)
+            lists.append(d_text.text)
+            swipe_x(driver)
+            time.sleep(2)
+        swipe_y(driver)
+    print("文章学习完成")
+    
+def look_vod(driver):
+    lists=[]
+    time.sleep(2)
+    print("aaaa")
+    move_to_vod(driver)
+    time.sleep(2)
+    while len(lists)<7:
+        time.sleep(2)
+        for_list = get_vod_list(driver)
+        for d_text in for_list:
+            if len(lists)>6:
+                break
+            d_text.click()
+            time.sleep(2)
+            print('正在打开',d_text.text)
+            swipe_y(driver)
+            time.sleep(240)
+            lists.append(d_text.text)
+            swipe_x(driver)
+            time.sleep(2)
+        swipe_y(driver)
+    print("视频学习完成")
+def print_log():
+    print("等待任务执行")
+def job():
+    print('正在打开夜神模拟器')
+    os.popen( r'E:\yeshen2\Nox\bin\Nox.exe ')
+    time.sleep(60)
+    print("正在打开appium")
+    res = subprocess.Popen('appium', shell=True)
+    time.sleep(5)
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', init_driver())
+    time.sleep(20)
+    look_text(driver)
+    move_to_index(driver)
+    look_vod(driver)
+    driver.quit()
+    print("任务完成关闭appium")
+    os.popen ("taskkill /im /F node.exe")
+    print("正在关闭夜神模拟器")
+    os.popen ("taskkill /F /im Nox.exe")
+    time.sleep(5)
+    text_list = np.array (all_of_text_list)
+    move_list = np.array (all_of_vod_list)
+    np.save ('text_db.npy',text_list)
+    np.save ('move_db.npy',move_list)
+    
+if __name__ == '__main__':
+    job()
+    
